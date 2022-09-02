7de11db9ea2e24585f82a6832172ca7bc0f15b1b
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-09-02 23:04:11 +0300

    updated dog.rb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
-
+    def name=(dog_name)
+        @this_dogs_name = dog_name    #instance variable
+       end
+   
+       def name
+        @this_dogs_name    #instance variable
+       end
+      
+   
+      lassie = Dog.new   #initializing a new object/instance
+      lassie.name = "Lassie"  #call instance method name on object lassie
+   
+      puts lassie.name
+   
 end
